0831030bd9a959bf6ec7e28446ab350e7fe6d377
Jinsoo Lim <jinsoo@jinsooui-MacBookPro.local>
2025-08-30 00:49:13 +0900

    update:Junit test. answerList of contents,@Transactional(DBSession)(20250829)

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -3,8 +3,10 @@ package com.mysite.sbb;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,12 +21,15 @@ class SbbApplicationTests {
     @Autowired
     private AnswerRepository answerRepository;
 
+    @Transactional
     @Test
     void testJpa() {
-        Optional<Answer> oa = this.answerRepository.findById(1);
-        assertTrue(oa.isPresent());
-        Answer a = oa.get();
-        assertEquals(102, a.getQuestion().getId());
+        Optional<Question> oq = this.questionRepository.findById(102);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+        List<Answer> answerList = q.getAnswerList();
+        assertEquals(1, answerList.size());
+        assertEquals("네, 자동으로 생성됩니다.", answerList.get(0).getContent());
 
     }
 }
