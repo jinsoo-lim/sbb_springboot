0f59fb77aad8532d7719d7478ecf205466d03d65
Jinsoo Lim <jinsoo@jinsooui-MacBookPro.local>
2025-08-30 00:25:03 +0900

    update:Junit test. answer of questionNum(20250829)

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -20,14 +21,10 @@ class SbbApplicationTests {
 
     @Test
     void testJpa() {
-        Optional<Question> oq = this.questionRepository.findById(102);
-        assertTrue(oq.isPresent());
-        Question q = oq.get();
+        Optional<Answer> oa = this.answerRepository.findById(1);
+        assertTrue(oa.isPresent());
+        Answer a = oa.get();
+        assertEquals(102, a.getQuestion().getId());
 
-        Answer a = new Answer();
-        a.setContent("네, 자동으로 생성됩니다.");
-        a.setQuestion(q);
-        a.setCreatDate(LocalDateTime.now());
-        this.answerRepository.save(a);
     }
 }
