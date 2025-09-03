64df595b28b730a1c3b6888d19f67e0efab529fd
Jinsoo Lim <jinsuzinsu@gmail.com>
2025-09-03 23:42:38 +0900

    update: AnswerService.create(20250903)

---
+++ src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -0,0 +1,28 @@
+package com.mysite.sbb.answer;
+
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionRepository;
+import com.mysite.sbb.question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerService {
+    private final AnswerRepository answerRepository;
+
+    public void create(Question question, String content) {
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setCreatDate(LocalDateTime.now());
+        answer.setQuestion(question);
+        this.answerRepository.save(answer);
+
+    }
+}
