5bfd446850bb706dced35ef63e2afdde7dc1a26c
jinsoo lim <jinsuzinsu@gmail.com>
2025-09-13 15:15:44 +0900

    300테스트 케이스 작성

--- src/main/java/com/mysite/sbb/SbbApplication.java
+++ src/main/java/com/mysite/sbb/SbbApplication.java
@@ -1,5 +1,6 @@
 package com.mysite.sbb;
 
+import groovy.transform.ASTTest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,6 @@ public class SbbApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SbbApplication.class, args);
 	}
+
 
 }
