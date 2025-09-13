5236fe81adf9b4cf065a674d7673069c3384a7c8
jinsoo lim <jinsuzinsu@gmail.com>
2025-09-13 16:36:22 +0900

    page, paging, getList(service), (20250913)

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -3,6 +3,7 @@ package com.mysite.sbb.question;
 import com.mysite.sbb.answer.AnswerForm;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,12 +18,24 @@ public class QuestionController {
 
     private final QuestionService questionService;
 
+    /*
+    paging.isEmpty	페이지 존재 여부를 의미한다(게시물이 있으면 false, 없으면 true).
+    paging.totalElements	전체 게시물 개수를 의미한다.
+    paging.totalPages	전체 페이지 개수를 의미한다.
+    paging.size	페이지당 보여 줄 게시물 개수를 의미한다.
+    paging.number	현재 페이지 번호를 의미한다.
+    paging.hasPrevious	이전 페이지의 존재 여부를 의미한다.
+    paging.hasNext	다음 페이지의 존재 여부를 의미한다.
+    */
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Question> questionList = this.questionService.getList();
-        model.addAttribute("questionList", questionList);
+    public String list(Model model, @RequestParam(value = "page", defaultValue ="0") int page) {
+//        List<Question> questionList = this.questionService.getList();
+        Page<Question> paging = this.questionService.getList(page);
+//        model.addAttribute("questionList", questionList);
+        model.addAttribute("paging", paging);
         return "question_list";
     }
+
 
     @GetMapping(value = "/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -16,11 +16,11 @@ import java.util.Optional;
 public class QuestionService {
     private final QuestionRepository questionRepository;
 
+/*
     public List<Question> getList() {
         return this.questionRepository.findAll();
-
     }
-
+*/
     public Question getQuestion(Integer id) {
         Optional<Question> question = this.questionRepository.findById(id);
         if (question.isPresent()) {
